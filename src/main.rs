b3feda1f8e54e8d9977e9146d3bb5738c26b52f8
nebelgrau <nebelgrau@gmx.com>
2020-07-19 15:18:49 +0200

    updated functions

--- src/main.rs
+++ src/main.rs
@@ -21,22 +21,109 @@ use stm32l0xx_hal::{
 
 use core::fmt::Write;
 
+const NUM_FEATURES: usize = 2;
+const NUM_EXAMPLES: usize = 3;
+const NUM_HIDDEN_NODES: usize = 3;
+const NUM_OUTPUT_NODES: usize = 1;
+
+pub type Float = f32;   //this way I can easily replace it with double precision f64 if necessary
+
 #[entry]
 fn main() -> ! {
     
     // neural network setup
 
-    //let input_vector: [f64; 3] = [30.0,87.0,110.0];  //[temperature, humidity, air_quality]
-    //let mut predicted_results:[f64; 3] = [0_f64; 3]; //initial array for predicted results
+    /*
+    let num_of_features = 2;
+    let num_of_examples = 3;
 
-    //let expected_values: [f64; 3] = [600.0, 10.0, -90.0]; //i.e. y values
+    let num_of_hidden_nodes = 3;
+    let num_of_output_nodes = 1;
+    */
+    
 
+    /*
+    train X:
+
+        2 5 1
+        8 5 8
+
+        dim = nx X m
+
+    */
+
+    let raw_x: [Float;NUM_FEATURES * NUM_EXAMPLES] = [2.0,5.0,1.0,
+                                                    8.0,5.0,8.0]; // raw_x[num_of_features][num_of examples]
+    
+
+    /*
+    train y:
+
+        200 90 190
+        
+        dim = 1 X m
+
+    */
+
+
+
+    let raw_y: [Float;NUM_EXAMPLES] = [200.0, 90.0, 190.0]; //raw_y[1][num]
+
+
+    let mut train_x = [0.0; NUM_FEATURES*NUM_EXAMPLES];
+    let mut train_y = [0.0;NUM_EXAMPLES];
+
+    let mut train_x_eg1 = [0.0; NUM_FEATURES];
+    let mut train_y_eg1 = 0.0;
+
+    let mut z1_eg1 = [0.0; NUM_HIDDEN_NODES];
+    let mut a1_eg1 = [0.0; NUM_HIDDEN_NODES];
+    let mut z2_eg1 = 0.0;
+    let mut yhat_eg1 = 0.0;
+
+    /*
+
+    // hours of workout data
+    let x1: [Float;3] = [2.0,5.0,1.0];
+
+    let mut x1_norm: [Float;3] = [0.0;3]; // normalized
+
+    // hours of rest data
+    let x2: [Float;3] = [8.0,5.0,8.0];
+
+    let mut x2_norm: [Float;3] = [0.0;3]; // normalized
+
+    // muscle gain (output)
+    let y: [Float;3] = [200.0, 90.0, 190.0];
+    
+    let mut y_norm: [Float;3] = [0.0;3]; // normalized
+    
+    */
+
+    // input layer to hidden layer weights matrix
+
+    let mut syn0 = [0.0; NUM_HIDDEN_NODES*NUM_FEATURES]; // number of hidden nodes * number of features, ideally [[f64;3];2]
+
+    // hidden layer to output layer weights matrix
+
+    let mut syn1 = [0.0; NUM_OUTPUT_NODES * NUM_HIDDEN_NODES]; // number of output nodes * number of hidden nodes
+
+
+
+
+    /*
+    let input_vector: [f64; 3] = [30.0,87.0,110.0];  //[temperature, humidity, air_quality]
+    let mut predicted_results:[f64; 3] = [0_f64; 3]; //initial array for predicted results
+
+    let expected_values: [f64; 3] = [600.0, 10.0, -90.0]; //i.e. y values
+    */
+    /*
     let mut weight: f64 = 0.5;
     let input: f64 = 0.5;
     let expected_value: f64 = 0.8;
     let step_amount: f64 = 0.001;
     let epochs: u32 = 1500;
-
+    */
     // peripherals setup
 
     let dp = pac::Peripherals::take().unwrap();
@@ -57,40 +144,65 @@ fn main() -> ! {
 
     let (mut tx, mut _rx) = lpserial.split();
 
+
+    
+    //let mut rng = SmallRng::seed_from_u64(0xdead_beef_cafe_d00d);
+
+    //let mut choice = rng.next_u32();
+    
+
     // 'AI' part comes here:
 
+    normalize_data(&raw_x, &mut train_x, NUM_EXAMPLES);
 
-    // error calculation done here: I want to move it to nn_lib.rs instead
-    
-    let mut prediction: f64 = 0.0;
-    let mut error: f64 = 0.0;
-    let mut up_prediction: f64 = 0.0;
-    let mut up_error: f64 = 0.0;
-    let mut down_prediction: f64 = 0.0;
-    let mut down_error: f64 = 0.0;
+    // brute_force_learning(input, weight, expected_value, step_amount, 1500, &mut tx);
 
-    for _ in 0..epochs {
-        prediction = input * weight;
-        error = find_error_simple(prediction, expected_value);
+    /*
+    normalize_data(&x1, &mut x1_norm, num_of_examples);
+    normalize_data(&x2, &mut x2_norm, num_of_examples);
+    normalize_data(&y, &mut y_norm, num_of_examples);
 
-        writeln!(tx, "Error: {:.3}      Prediction: {:.3}\r\n", error, prediction).unwrap();
+    writeln!(tx, "\nRaw x1 data:\t").unwrap();
 
-        up_prediction = input * (weight + step_amount);
-        up_error = find_error_simple(up_prediction, expected_value);
-
-        down_prediction = input * (weight - step_amount);
-        down_error = find_error_simple(down_prediction, expected_value);
-
-        if down_error < up_error {
-            weight = weight - step_amount;
-        }
-
-        if down_error > up_error {
-            weight = weight + step_amount;
-        }
-
+    for i in 0..num_of_examples {
+        write!(tx, "{}\t", x1[i]).unwrap();
     }
 
+    writeln!(tx, "\nNormalized x1 data:\t").unwrap();
+
+    for i in 0..num_of_examples {
+        write!(tx, "{}\t", x1_norm[i]).unwrap();
+    }
+
+    // repeat for x2 and y
+
+    */
+
+    /*
+
+    weights_random_initialization(NUM_HIDDEN_NODES, NUM_FEATURES, &mut syn0);
+    weights_random_initialization(NUM_OUTPUT_NODES, NUM_HIDDEN_NODES, &mut syn1);
+
+    // synapse 0 weights
+    
+    writeln!(tx, "\nSynapse 0 weights (input to hidden):").unwrap();
+    
+    for i in 0..NUM_HIDDEN_NODES {
+        for j in 0..NUM_FEATURES {
+            writeln!(tx, "{}\t", syn0[i*NUM_FEATURES + j]).unwrap();
+        }
+    }
+
+    writeln!(tx, "\nSynapse 1 weights (hidden to output):").unwrap();
+
+    // synapse 1 weights
+    for i in 0..NUM_OUTPUT_NODES {
+        for j in 0..NUM_HIDDEN_NODES {
+            writeln!(tx, "{}\t", syn0[i*NUM_HIDDEN_NODES + j]).unwrap();
+        }
+    }
+
+    */
 
     loop {}
 
--- src/nn_lib.rs
+++ src/nn_lib.rs
@@ -1,10 +1,21 @@
+//pub type Float = f32;   //this way I can easily replace it with double precision f64 if necessary
+
 pub mod nn {
 
-    fn weighted_sum(input: [f64;3], weight: [f64; 3], input_len: u32) -> f64 {
 
-        let mut output: f64 = 0.0;
+    /*
 
-        for i in 0..input_len as usize {
+    fn square(num: Float) -> Float {        // just a test of the type alias
+        return num * num;
+    }
+
+    */
+
+    fn weighted_sum(input: [f32;3], weight: [f32; 3], input_len: usize) -> f32 {
+
+        let mut output: f32 = 0.0;
+
+        for i in 0..input_len {
             output += input[i] * weight[i];
         }
 
@@ -12,9 +23,9 @@ pub mod nn {
 
     }
 
-    pub fn multiple_inputs_single_output_nn(input: [f64;3], weight: [f64; 3], input_len: u32) -> f64 {
+    pub fn multiple_inputs_single_output_nn(input: [f32;3], weight: [f32; 3], input_len: usize) -> f32 {
 
-        let mut predicted_value: f64 = 0.0;
+        let mut predicted_value: f32 = 0.0;
 
         predicted_value = weighted_sum(input, weight, input_len);
 
@@ -22,7 +33,7 @@ pub mod nn {
 
     }
 
-    fn elementwise_multiply(input_scalar: f64, weight_vector: &[f64], output_vector: &mut [f64], vector_len: usize) {
+    fn elementwise_multiply(input_scalar: f32, weight_vector: &[f32], output_vector: &mut [f32], vector_len: usize) {
 
         for i in 0..vector_len {
             output_vector[i] = input_scalar * weight_vector[i];
@@ -30,18 +41,18 @@ pub mod nn {
 
     }
 
-    pub fn single_in_multiple_out_nn(input_scalar: f64, weight_vector: &[f64], output_vector: &mut [f64], vector_len: usize) {
+    pub fn single_in_multiple_out_nn(input_scalar: f32, weight_vector: &[f32], output_vector: &mut [f32], vector_len: usize) {
 
         elementwise_multiply(input_scalar, &weight_vector, output_vector, vector_len);
 
     } 
 
 
-    fn matrix_vector_multiplication(input_vector: &[f64], 
+    fn matrix_vector_multiplication(input_vector: &[f32], 
                                     input_len: usize, 
-                                    output_vector: &mut [f64], 
+                                    output_vector: &mut [f32], 
                                     output_len: usize, 
-                                    weights_matrix: &[f64]) {
+                                    weights_matrix: &[f32]) {
         
         for k in 0..output_len {
             for i in 0..input_len {
@@ -52,70 +63,88 @@ pub mod nn {
 
 
 
-    pub fn multiple_in_multiple_out(input_vector: &[f64], 
+    pub fn multiple_in_multiple_out(input_vector: &[f32], 
         input_len: usize, 
-        output_vector: &mut [f64], 
+        output_vector: &mut [f32], 
         output_len: usize, 
-        weights_matrix: &[f64]) {
+        weights_matrix: &[f32]) {
     
         matrix_vector_multiplication(&input_vector, input_len, output_vector, output_len, &weights_matrix);
 
     }
 
     
-
-    pub fn hidden_nn (input_vector: &[f64],
+    /*
+    pub fn hidden_nn (input_vector: &[f32],
         input_len: usize,
         hidden_len: usize,
-        input_to_hidden_weights: &[f64],
+        input_to_hidden_weights: &[f32],
         output_len: usize,
-        hidden_to_output_weights: &[f64],
-        output_vector: &mut [f64]){
+        hidden_to_output_weights: &[f32],
+        output_vector: &mut [f32]){
 
-        let mut hidden_predicted: [f64; 3] = [0_f64; 3];
+                    
+        let mut hidden_predicted = [0f32; 3]; //this needs to be fixed; must use vector and not array
+        
         matrix_vector_multiplication(&input_vector, input_len, &mut hidden_predicted, output_len, input_to_hidden_weights);
         matrix_vector_multiplication(&hidden_predicted, hidden_len, output_vector, output_len, hidden_to_output_weights);
 
         }
+    */
+
+    pub fn hidden_nn (input_vector: &[f32],
+        input_len: usize,
+        hidden_len: usize,
+        input_to_hidden_weights: &[f32],
+        output_len: usize,
+        hidden_to_output_weights: &[f32],
+        mut buffer: &mut [f32], // empty buffer of the same size as hidden 
+        output_vector: &mut [f32]){
 
 
-    pub fn find_error_simple(yhat: f64, y: f64) -> f64 {
+        matrix_vector_multiplication(&input_vector, input_len, &mut buffer, output_len, input_to_hidden_weights);
+        matrix_vector_multiplication(&buffer, hidden_len, output_vector, output_len, hidden_to_output_weights);
 
-        let temp: f64 = yhat-y;
+        }
+
+    
+
+
+
+    pub fn find_error_simple(yhat: f32, y: f32) -> f32 {
+
+        let temp: f32 = yhat-y;
         return temp * temp; //x.powf(y) not available in no_std
         
     }
 
-    pub fn find_error(input: f64, weight: f64, expected_value: f64) -> f64 {
+    pub fn find_error(input: f32, weight: f32, expected_value: f32) -> f32 {
 
-        let temp: f64 = (input * weight) - expected_value; 
+        let temp: f32 = (input * weight) - expected_value; 
         return temp * temp; //x.powf(y) not available
         
     }
 
     
-    
-    // this won't work, needs access to serial tx
-    
-    pub fn brute_force_learning(input: f64,
-                                mut weight: f64,
-                                expected_value: f64,
-                                step_amount: f64,
+    pub fn brute_force_learning(input: f32,
+                                mut weight: f32,
+                                expected_value: f32,
+                                step_amount: f32,
                                 epochs: u32, 
-                                tx: stm32l0xx_hal::serial::Tx<stm32l0::stm32l0x1::LPUART1> ) {
+                                tx: &mut stm32l0xx_hal::serial::Tx<stm32l0::stm32l0x1::LPUART1> ) {
 
-        let mut prediction: f64 = 0.0;
-        let mut error: f64 = 0.0;
-        let mut up_prediction: f64 = 0.0;
-        let mut up_error: f64 = 0.0;
-        let mut down_prediction: f64 = 0.0;
-        let mut down_error: f64 = 0.0;
+        let mut prediction: f32 = 0.0;
+        let mut error: f32 = 0.0;
+        let mut up_prediction: f32 = 0.0;
+        let mut up_error: f32 = 0.0;
+        let mut down_prediction: f32 = 0.0;
+        let mut down_error: f32 = 0.0;
 
         for _ in 0..epochs {
             prediction = input * weight;
             error = find_error_simple(prediction, expected_value);
-
-            writeln!(tx, "Error: {:.3}      Prediction: {:.3}\r\n", error, prediction).unwrap(); //this won't work
+            use core::fmt::Write;
+            writeln!(tx, "Error: {:.3}      Prediction: {:.3}\r\n", error, prediction).unwrap(); 
 
             up_prediction = input * (weight + step_amount);
             up_error = find_error_simple(up_prediction, expected_value);
@@ -138,7 +167,64 @@ pub mod nn {
     }
 
 
+    pub fn normalize_data(input_vector: &[f32], output_vector: &mut [f32], len: usize) {
+        //find max
+        let mut max: f32 = input_vector[0];
+        for i in 0..len {
+            if(input_vector[i]) > max {
+                max = input_vector[i];
+            }
+        }
+
+        // normalize: divide each item by max
+        for i in 0..len {
+            output_vector[i] = input_vector[i] / max;
+        }
+
     }
 
 
+    use rand::prelude::*;
 
+    const U32_MAX: f32 = 4294967295.0; // u32::MAX
+
+    pub fn weights_random_initialization(hidden_len: usize, input_len: usize, weights_matrix: &mut [f32]) {
+        
+        //weights_matrix[hidden_len][input_len]
+
+        let mut rng = SmallRng::seed_from_u64(0xdead_beef_cafe_d00d);
+
+        for i in 0..hidden_len {
+            for j in 0..input_len {
+
+                // generate random numbers between 0 and 1
+
+                let mut rand_val = rng.next_u32() as f32;
+
+                rand_val = rand_val / U32_MAX;
+
+                weights_matrix[i*input_len + j] = rand_val;
+                
+
+            }
+
+        }
+
+    }
+
+
+    use micromath::F32Ext;      // used for exp function
+
+    fn sigmoid(x: f32) -> f32 {
+        let result: f32 = 1.0 / (1.0 + F32Ext::exp(-x));
+
+        result
+    }
+
+    pub fn vector_sigmoid(input_vector: &[f32], output_vector: &mut [f32], len: usize) {
+        for i in 0..len {
+            output_vector[i] = sigmoid(input_vector[i]);
+        }
+    }
+
+}
